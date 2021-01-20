c078aecb26bb9854f8a5693dfbbf4e03c89e3de1
Cherno Badjan <badjancherno@gmail.com>
2021-01-20 14:34:12 -0800

    Step 2: Cart data

--- cart/cart-data.js
+++ cart/cart-data.js
@@ -1,6 +1,18 @@
 export const cart = [
     {
-        id: 3,
-        quantity: 4
+        id: 1,
+        quantity: 4,
     },
-]
+    {
+        id: 2,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        quantity: 1
+    },
+    {
+        id: 4,
+        quantity: 2,
+    },
+];
